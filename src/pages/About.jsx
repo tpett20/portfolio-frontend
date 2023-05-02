337f0a36796b81483edd61d43d52b8ae382ea7d5
tpett20 <thomaspettersen20@gmail.com>
2023-05-02 15:17:43 -0400

    set up About page to fetch and render local data from JSON file.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,5 +1,30 @@
+import { useState, useEffect } from "react"
+
 function About(props) {
-    return <h1>About</h1>
+    const [about, setAbout] = useState(null)
+
+    async function getAboutData() {
+        fetch('./about.json')
+            .then(response => response.json())
+            .then(json => setAbout(json))
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        getAboutData()
+    }, [])
+
+    function loaded() {
+        return (
+            <div>
+                <h2>{about.name}</h2>
+                <h3>{about.email}</h3>
+                <p>{about.bio}</p>
+            </div>
+        )
+    }
+
+    return about ? loaded() : <h1>Loading...</h1>
 }
 
 export default About
