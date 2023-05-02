ba6ae4465f5de7d6b4c65e23d120b1e88926a4b3
tpett20 <thomaspettersen20@gmail.com>
2023-05-02 15:03:03 -0400

    add basic navbar to header with links to each page.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,23 @@
+import { Link } from "react-router-dom"
+import './Header.css'
+
 function Header(props) {
-    return <h1>Header</h1>
+    return (
+        <header>
+            <h1>Tom's Portfolio</h1>
+            <nav className="Navbar">
+                <Link to='/'>
+                    <div>Home</div>
+                </Link>
+                <Link to='/about'>
+                    <div>About</div>
+                </Link>
+                <Link to='/projects'>
+                    <div>Projects</div>
+                </Link>
+            </nav>
+        </header>
+    )
 }
 
 export default Header
