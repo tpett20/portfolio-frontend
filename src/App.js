768078d49711bb444d48097364d85aabf8661b5f
tpett20 <thomaspettersen20@gmail.com>
2023-05-02 14:54:10 -0400

    set up App.js by importing all components and stub routes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
 import './App.css';
+// Components
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Route, Routes } from 'react-router';
+// Pages
+import Home from './pages/Home';
+import About from './pages/About';
+import Projects from './pages/Projects';
 
 function App() {
+  const URL = "http://localhost:4000/"
+
   return (
-    <h1>Hello</h1>
-  );
+    <div>
+      <Header />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/projects' element={<Projects URL={URL} />} />
+        <Route path='/about' element={<About URL={URL} />} />
+      </Routes>
+      <Footer />
+    </div>
+  )
 }
 
 export default App;
