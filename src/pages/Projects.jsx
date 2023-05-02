4666a5b936a95f53f06529d25d939f2436e4380a
tpett20 <thomaspettersen20@gmail.com>
2023-05-02 15:26:52 -0400

    set up Projects page to fetch and render local data from JSON file.

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,5 +1,35 @@
+import { useState, useEffect } from "react"
+
 function Projects(props) {
-    return <h1>Projects</h1>
+    const [projects, setProjects] = useState(null)
+
+    async function getProjectsData() {
+        fetch('./projects.json')
+            .then(response => response.json())
+            .then(json => setProjects(json))
+            .catch(error => console.log(error))
+    }
+
+    useEffect(() => {
+        getProjectsData()
+    }, [])
+
+    function loaded() {
+        return projects.map(project => (
+            <div>
+                <h1>{project.name}</h1>
+                <img src={project.image} alt={`${project.name} Screenshot`} />
+                <a href={project.git}>
+                    <button>GitHub</button>
+                </a>
+                <a href={project.live}>
+                    <button>Live Site</button>
+                </a>
+            </div>
+        ))
+    }
+
+    return projects ? loaded() : <h1>Loading...</h1>
 }
 
 export default Projects
