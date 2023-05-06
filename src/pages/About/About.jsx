84c80d4ce665fef21c395d302c63e6578a743912
tpett20 <thomaspettersen20@gmail.com>
2023-05-06 15:12:15 -0400

    style about page and include skills cards.

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -19,9 +19,63 @@ function About(props) {
     function loaded() {
         return (
             <>
-                <h2>{about.name}</h2>
-                <h3>{about.email}</h3>
-                <p>{about.bio}</p>
+                <h1>About</h1>
+                <div className="row">
+                    <div className="col-12 col-xl-9 col-xxl-8">
+                        <p>{about.bio}</p>
+                    </div>
+                </div>
+                <h3>Skills</h3>
+                <div className="row row-cols-1 row-cols-md-2 g-4 mb-3">
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header text-bg-secondary fw-bold">
+                                Languages
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                {about.languages.map((l, idx) => (
+                                    <li className="list-group-item" key={idx}>{l}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header text-bg-secondary fw-bold">
+                                Frameworks and Libraries
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                {about.frameworks.map((f, idx) => (
+                                    <li className="list-group-item" key={idx}>{f}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header text-bg-secondary fw-bold">
+                                Databases
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                {about.databases.map((d, idx) => (
+                                    <li className="list-group-item" key={idx}>{d}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                    <div className="col">
+                        <div className="card">
+                            <div className="card-header text-bg-secondary fw-bold">
+                                Other
+                            </div>
+                            <ul className="list-group list-group-flush">
+                                {about.other.map((o, idx) => (
+                                    <li className="list-group-item" key={idx}>{o}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </>
         )
     }
