f7eb19586ea419d29729b81c2b0b01f2caa1b7d3
tpett20 <thomaspettersen20@gmail.com>
2023-05-19 16:11:15 -0400

    add Capstone project, update resume file

--- src/profileData/projects.js
+++ src/profileData/projects.js
@@ -1,9 +1,17 @@
 const projectsData = [
     {
+        name: "SearchCast",
+        live: "https://searchcast.netlify.app/",
+        git: "https://github.com/tpett20/SearchCast-frontend",
+        image: "https://i.imgur.com/jgx0nwo.png",
+        info: "An app to help you track keyword searches for Spotify podcast episodes.",
+        technologies: "MERN Stack, Spotify Web API, Auth0, Bootstrap"
+    },
+    {
         name: "Chorderly",
         live: "https://tpett20.github.io/chorderly-game/",
         git: "https://github.com/tpett20/chorderly-game",
-        image: "https://i.imgur.com/lA1GJf9.png",
+        image: "https://i.imgur.com/MKaqI6S.png",
         info: "A memory game designed to help you learn musical chords by ear.",
         technologies: "Javascript, HTML, CSS, DOM Manipulation"
     },
@@ -11,7 +19,7 @@ const projectsData = [
         name: "StubSaver",
         live: "http://stubsaver.herokuapp.com/",
         git: "https://github.com/tpett20/StubSaver",
-        image: "https://i.imgur.com/REnGyKP.png",
+        image: "https://i.imgur.com/dF23Kou.png",
         info: "An app for saving the concerts and sporting events you've attended.",
         technologies: "Django, Python, PostgreSQL, Bulma"
     },
@@ -19,7 +27,7 @@ const projectsData = [
         name: "Break the Ice",
         live: "https://break-the-ice-app.herokuapp.com/icebreakers",
         git: "https://github.com/tpett20/break-the-ice-app",
-        image: "https://i.imgur.com/nSwfYyC.png",
+        image: "https://i.imgur.com/SE99KGc.png",
         info: "An app for storing icebreaker questions and responses.",
         technologies: "Express.js, Node.js, MongoDB, Mongoose, Bootstrap"
     }
