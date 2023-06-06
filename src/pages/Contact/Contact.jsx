86e3ea6e7daa89f7ee7b30ee1eac187bb0a0bf64
tpett20 <thomaspettersen20@gmail.com>
2023-06-06 15:03:42 -0400

    fix height of Contact form textarea

--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,3 +1,4 @@
+import './Contact.css'
 import { useRef } from "react"
 import emailjs from "@emailjs/browser"
 
@@ -30,7 +31,7 @@ export default function Contact() {
                     <div className="card-body">
                         <div className="d-flex justify-content-between align-items-center text-break">
                             <h5 className="card-title mb-0">ThomasPettersen20@gmail.com</h5>
-                            <button onClick={copyText} className="btn btn-sm btn-outline-secondary ms-2 d-none d-sm-block">
+                            <button onClick={copyText} className="btn btn-sm btn-outline-secondary ms-2 d-none d-sm-block copy-btn">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-files" viewBox="0 0 16 16">
                                     <path d="M13 0H6a2 2 0 0 0-2 2 2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h7a2 2 0 0 0 2-2 2 2 0 0 0 2-2V2a2 2 0 0 0-2-2zm0 13V4a2 2 0 0 0-2-2H5a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1zM3 4a1 1 0 0 1 1-1h7a1 1 0 0 1 1 1v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V4z" />
                                 </svg>
