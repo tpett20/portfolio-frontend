77d935eb1f22ab6ef73936325d82fd3cc87f8762
tpett20 <thomaspettersen20@gmail.com>
2023-08-07 13:50:16 -0400

    update skills and bio on About page, update Resume file

--- src/profileData/about.js
+++ src/profileData/about.js
@@ -1,11 +1,11 @@
 const aboutData = {
     name: "Thomas Pettersen",
     email: "ThomasPettersen20@gmail.com",
-    bio: "I am a Software Engineer who thrives on problem solving, learning, and teamwork. Previously, I’ve owned and operated a furniture importing company, developing skills of self-reliance and project management. I’m excited to leverage my experience to become an invaluable part of my next team’s ability to tackle any challenge. ",
-    languages: ["JavaScript", "Python", "HTML", "CSS", "EJS", "SQL"],
-    frameworks: ["Express.js", "Django", "React", "Bootstrap", "Mongoose"],
+    bio: "I am a Software Engineer who excels in problem solving. I specialize in teamwork, collaboration, ideation, and execution of complex projects across industries. Through my experience operating a furniture importing company, I am leveraging skills in project management, sales, and operations. ",
+    languages: ["JavaScript", "Python", "HTML", "CSS", "SQL", "EJS"],
+    frameworks: ["React", "Express.js", "Django", "Bootstrap", "Mongoose"],
     databases: ["MongoDB", "PostgreSQL"],
-    other: ["Node.js", "RESTful Routing", "Git", "Debugging", "Algorithms"]
+    other: ["Node.js", "Git", "Postman"]
 }
 
 export { aboutData }
