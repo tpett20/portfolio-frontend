4c0424d1f091df18bd12a1b72c739cda1a7cf48f
tpett20 <thomaspettersen20@gmail.com>
2023-12-04 16:59:39 -0500

    update about page bio statement

--- src/profileData/about.js
+++ src/profileData/about.js
@@ -1,7 +1,7 @@
 const aboutData = {
     name: "Thomas Pettersen",
     email: "ThomasPettersen20@gmail.com",
-    bio: "I am a Software Engineer who excels in problem solving. I specialize in teamwork, collaboration, ideation, and execution of complex projects across industries. Through my experience operating a furniture importing company, I am leveraging skills in project management, sales, and operations. ",
+    bio: "I am a Software Engineer who excels in problem solving. Through my experience as a coding instructor, I possess a strong ability to communicate technical concepts. I’m also experienced in cross-industry collaboration, leading complex projects with cross-functional teams. I’m skilled in product lifecycle management through founding, scaling, and operating a product sales business, coordinating sales, operations and project management.",
     languages: ["JavaScript", "Python", "HTML", "CSS", "SQL", "EJS"],
     frameworks: ["React", "Express.js", "Django", "Bootstrap", "Mongoose"],
     databases: ["MongoDB", "PostgreSQL"],
